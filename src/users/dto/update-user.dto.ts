217baba4ee50f0a6b7e15911314d1dde6e74a750
gadzilaaa3 <98168491+gadzilaaa3@users.noreply.github.com>
2023-10-06 13:59:50 +0300

    Add set roles dtos

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { Role } from 'src/roles/enums/role.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  roles?: Role[];
+}
